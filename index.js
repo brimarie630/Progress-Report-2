3ab22c73be365411d60fee4caaef595089dbb2e3
Brianna Mosholder <bmoshold@umich.edu>
2023-02-17 23:24:28 -0500

    first commit

--- index.js
+++ index.js
@@ -29,26 +29,70 @@ client.on("messageCreate", (message) => {
     if (command === 'test') {
         message.channel.send("Bot is working!");
     }else if(command === 'cat_create'){
-        if(argument.length!=1){
-            message.channel.send("Incorrect amount of arguments. Arguments: cat_name.");
+        if(argument.length!=2){
+            message.channel.send("Incorrect amount of arguments. Arguments: cat_name, role_name.");
             return;
         }
         //Grab name
         var cat_name = argument[0];
 
+        var role_name = argument[1];
+
+        var roleID = message.guild.roles.cache.find((r) => r.name === role_name);
+
         //Create Category
         message.guild.channels.create({
-            name: 'new-general',
-            type: 2
-        })
+            name: cat_name,
+            type: 4,
+            permissionOverwrites: [
+                {
+                    id: message.guild.id,
+                    deny: [PermissionsBitField.Flags.ViewChannel],
+                },
+                {
+                    id: '1076352621532696586',
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                },
+            ],
+        });
 
         //Victory
         message.channel.send("Created category named: " + cat_name);
+
+        //Create Channels
+        //announcements
+        //zoom-meeting-info
+        //introduce-yourself
+        //chat
+
+        var catID = message.guild.channels.cache.find((c) => c.name === cat_name);
+        message.guild.channels.create({
+            name: 'announcements',
+            type: 2,
+            parent: catID,
+        });
+
+
+    }else if(command === 'chan_create'){
+        if(argument.length!=1){
+            message.channel.send("Incorrect amount of arguments. Arguments: chan_name.");
+            return;
+        }
+        //Grab name
+        var chan_name = argument[0];
+
+        //Create Channel
+        message.guild.channels.create({
+            name: chan_name,
+            type: 2,
+        });
+
+        //Victory
+        message.channel.send("Created category named: " + chan_name);
     }
+});
+
+       
 
 
-})
-
-
-
-client.login("Token");
+client.login("token");
